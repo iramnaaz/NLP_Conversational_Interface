8f10a5766a3ff60e6e05bf876d99ca58e3568262
iramnaaz <55992402+iramnaaz@users.noreply.github.com>
2020-03-12 23:13:40 -0500

    Removing Non Letters from the String(Question)
    Iram Naaz Khan

--- whatFunction.py
+++ whatFunction.py
@@ -1,18 +1,30 @@
 from PyDictionary import PyDictionary
 
+UPPERLETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+LETTERS_AND_SPACE = UPPERLETTERS + UPPERLETTERS.lower() + ' \t\n'
+
+def removeNonLetter(ques):
+    lettersOnly=[]
+    for symbol in ques:
+        if symbol in LETTERS_AND_SPACE:
+            lettersOnly.append(symbol)
+    return ''.join(lettersOnly)
 
 def whatIs(ques):
-    result= ques.split(" ")
+    NonLetterWords=removeNonLetter(ques)
+    result= NonLetterWords.split(" ")
     #print(result[0])
     dictionary = PyDictionary()
     if result[0].lower().__contains__('what'):
         #print("yes")
         #print(result[2])
+
         meaning = dictionary.meaning(result[2])
         print(meaning)
     else:
         print("Question must Start with What dear..!")
     return result
+
 
 
 
