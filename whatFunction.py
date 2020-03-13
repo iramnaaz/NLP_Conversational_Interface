5061eef63f4d11c62d745407b4dd92c1c10d4b72
iramnaaz <55992402+iramnaaz@users.noreply.github.com>
2020-03-12 23:15:47 -0500

    Checking Whether the String is in English or not
    Iram Naaz Khan

--- whatFunction.py
+++ whatFunction.py
@@ -1,3 +1,4 @@
+import enchant
 from PyDictionary import PyDictionary
 
 UPPERLETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -14,18 +15,25 @@ def whatIs(ques):
     NonLetterWords=removeNonLetter(ques)
     result= NonLetterWords.split(" ")
     #print(result[0])
+    for i in result:
+        isEnglish(i)
     dictionary = PyDictionary()
     if result[0].lower().__contains__('what'):
         #print("yes")
         #print(result[2])
-
         meaning = dictionary.meaning(result[2])
         print(meaning)
     else:
         print("Question must Start with What dear..!")
     return result
 
-
+def isEnglish(message):
+    d = enchant.Dict("en_US")
+    if d.check(message) == True:
+        return
+    else:
+        print("Type only English Words dear!")
+        exit()
 
 
 def main():
