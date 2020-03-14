8d5dfa0a89ccc9b432487d952c079baeb2cd6dd1
Zachary Kornbluth <zkornbluth2007@gmail.com>
2020-03-14 10:14:51 -0500

    Simple how to function

---
+++ howToFunction.py
@@ -0,0 +1,11 @@
+def howTo(question):
+	question_words = question.split()
+
+	search_url = 'google.com/search?q=' + '+'.join(question_words)
+
+	return_term = 'No worries, I found a reference for you: ' + search_url
+
+	return return_term
+
+
+print(howTo('How do I turn on an oven?'))
